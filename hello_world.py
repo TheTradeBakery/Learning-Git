be77db6c3d1ad8271aa2126ec8855b9d606bff2e
The Trade Bakery <thetradebakery@gmail.com>
2023-02-12 14:15:13 -0500

    git with syncthing

--- hello_world.py
+++ hello_world.py
@@ -3,5 +3,5 @@ print("hello world")
 
 # Test Change Number 2
 # Testing what happens when two people edit the same file at the same time 
-for pie in range(8):
+for pie in range(9):
     print(pie)
