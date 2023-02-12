36da5919cfaf8470c0fb1c572fbeb267b49576bd
TheTradeBakery <45054378+TheTradeBakery@users.noreply.github.com>
2023-02-12 11:58:06 -0500

    Update hello_world.py
    still trying to figure out merging

--- hello_world.py
+++ hello_world.py
@@ -3,5 +3,5 @@ print("hello world")
 
 # Test Change Number 2
 # Testing what happens when two people edit the same file at the same time 
-for pie in range(6):
+for pie in range(5):
     print(pie)
