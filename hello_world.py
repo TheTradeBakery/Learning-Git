695e630ecdebbec88a985ccd096d8d219602e526
The Trade Bakery <thetradebakery@gmail.com>
2023-02-12 11:53:22 -0500

    merged file

--- hello_world.py
+++ hello_world.py
@@ -3,6 +3,5 @@ print("hello world")
 
 # Test Change Number 2
 # Testing what happens when two people edit the same file at the same time 
-# Creating another conflit to test merging
-for pie in range(5):
+for pie in range(6):
     print(pie)
